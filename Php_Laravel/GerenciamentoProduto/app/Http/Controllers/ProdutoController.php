0b024c5b3e7feab91685f2a779610852a5fe7d7a
anthonyrct <anthonyteixaira@gmail.com>
2024-08-06 16:43:07 -0300

    06/08/24

---
+++ Php_Laravel/GerenciamentoProduto/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produto;
+
+class ProdutoController extends Controller
+{
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('produtos', compact('produtos'));
+    }
+
+    public function create()
+    {
+        return view('produtos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required|decimal',
+        ]);
+
+        Produto::create($request->all());
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto criado com sucesso.');
+    }
+
+    public function show(Produto $produto)
+    {
+        return view('produtos.show', compact('produto'));
+    }
+
+    public function edit(Produto $produto)
+    {
+        return view('produto.edit', compact('produto'));
+    }
+
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required|decimal',
+        ]);
+        $produto->update($request->all());
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto atualizado com sucesso.');
+    }
+
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto Deletado com Sucesso.');
+    }
+
+}
