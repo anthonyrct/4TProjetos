44c83ff5e25c1b32fe41298dd614cc8e5eb9a1bd
anthonyrct <anthonyteixaira@gmail.com>
2024-08-22 16:50:30 -0300

    22/08/24

--- Php_Laravel/EnsinoDistancia2/app/Models/Usuario.php
+++ Php_Laravel/EnsinoDistancia2/app/Models/Usuario.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome', 'email', 'password', 'role'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    // Relacionamentos
+    public function cursos()
+    {
+        return $this->hasMany(Cursos::class, 'user_id');
+    }
+
+    public function matriculas()
+    {
+        return $this->hasMany(Matricula::class, 'user_id');
+    }
 }
--- Php_Laravel/EnsinoDistancia2/database/migrations/2024_08_22_191414_create_usuarios_table.php
+++ Php_Laravel/EnsinoDistancia2/database/migrations/2024_08_22_191414_create_usuarios_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['student', 'teacher']);
             $table->timestamps();
         });
     }
